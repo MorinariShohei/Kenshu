d94fdcd5caab390209fc86fc4445a5914e454e81
SHoheiMorinari <ec0993rk@ed.ritsumei.ac.jp>
2023-09-17 13:01:45 +0900

    [Finish]Webアプリケーションを作ってみよう-2

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -5,6 +5,11 @@ let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
 
+if (username === null || username.trim() === "") {
+  username = "名無し";
+}
+document.getElementById("name").innerHTML = username;
+
 let rand = Math.floor( Math.random() * 5);
 if (rand == 0) {
   userresult = "大吉";
@@ -22,3 +27,4 @@ if (rand == 4) {
   userresult = "凶";
 }
 document.getElementById("result").innerHTML = userresult;
+
